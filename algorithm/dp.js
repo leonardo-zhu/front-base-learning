9de385ebca600f0cc4d2a381f9bc535c84350e8d
xiaolong.zxl <xiaolong.zxl@quadtalent.com>
2021-02-15 18:00:21 +0800

    opt: dp:climbStairs

--- algorithm/dp.js
+++ algorithm/dp.js
@@ -5,19 +5,23 @@
 
 /**
  * 爬楼梯
+ *
  * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
  * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/climbing-stairs/
  * @param n
  * 台阶数
  */
-function climbStairs(n){
+function climbStairs(n) {
   const dp = []
   dp[0] = 0
   dp[1] = 1
   dp[2] = 2
 
   for (let i = 0; i < n; i++) {
-    dp[n] = dp[i-1] + dp[i-2]
+    dp[n] = dp[i - 1] + dp[i - 2]
   }
 
   return dp[n]
