d2997ab0ae84149b262c28a66cf6f3178a137702
xiaolong.zxl <xiaolong.zxl@quadtalent.com>
2021-02-15 18:00:48 +0800

    feat: dp:uniquePaths

--- algorithm/dp.js
+++ algorithm/dp.js
@@ -26,3 +26,37 @@ function climbStairs(n) {
 
   return dp[n]
 }
+
+/**
+ * 不同路径
+ *
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ * 问总共有多少条不同的路径？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/unique-paths
+ * @param m
+ * 横
+ * @param n
+ * 竖
+ */
+function uniquePaths(m, n) {
+  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+
+  for (let i = 0; i < m; i++) {
+    dp[i][0] = 1
+  }
+
+  for (let i = 0; i < n; i++) {
+    dp[0][i] = 1
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    }
+  }
+
+  return dp[m - 1][n - 1]
+}
